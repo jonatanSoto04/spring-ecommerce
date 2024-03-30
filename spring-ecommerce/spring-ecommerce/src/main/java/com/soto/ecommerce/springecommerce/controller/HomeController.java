ee64c52ba0e7dc90d2397d251dd5666644df5272
Jonatan Soto <jonatan.sotop.04@gmail.com>
2024-03-30 14:38:10 -0500

    Obtener la cantidad y buscar el producto en el home controller para añadir al carrito

--- spring-ecommerce/spring-ecommerce/src/main/java/com/soto/ecommerce/springecommerce/controller/HomeController.java
+++ spring-ecommerce/spring-ecommerce/src/main/java/com/soto/ecommerce/springecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.soto.ecommerce.springecommerce.controller;
 
+import com.soto.ecommerce.springecommerce.model.DetailOrder;
+import com.soto.ecommerce.springecommerce.model.Order;
 import com.soto.ecommerce.springecommerce.model.Product;
 import com.soto.ecommerce.springecommerce.service.ProductService;
 import org.slf4j.Logger;
@@ -7,11 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -20,6 +21,10 @@ public class HomeController {
     private final Logger log = LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private ProductService productService;
+    //Para almacenar los detalles de la orden
+    List<DetailOrder> detail = new ArrayList<DetailOrder>();
+    //datos de la orden
+    Order order = new Order();
     @GetMapping("")
     public String home(Model model){
         model.addAttribute("products", productService.findAll());
@@ -35,8 +40,19 @@ public class HomeController {
         return "user/producthome";
     }
     @PostMapping("cart")
-    public String addCart(){
+    public String addCart(@RequestParam Integer id, @RequestParam Integer amount){
+        DetailOrder detailOrder = new DetailOrder();
+        Product product = new Product();
+        double sumTotal = 0;
+
+        Optional<Product> optionalProduct = productService.get(id);
+        log.info("Producto añaido: {}", optionalProduct.get());
+        log.info("Cantidad: {}", amount);
+
+
         return "user/trolley";
     }
+
+
 
 }
