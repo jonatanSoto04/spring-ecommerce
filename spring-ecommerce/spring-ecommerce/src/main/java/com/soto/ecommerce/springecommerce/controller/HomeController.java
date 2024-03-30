ce029139ef227b5f259ba68df94e150c06a61428
Jonatan Soto <jonatan.sotop.04@gmail.com>
2024-03-30 13:48:46 -0500

    mostrar informacion del producto en la pagina productohome

--- spring-ecommerce/spring-ecommerce/src/main/java/com/soto/ecommerce/springecommerce/controller/HomeController.java
+++ spring-ecommerce/spring-ecommerce/src/main/java/com/soto/ecommerce/springecommerce/controller/HomeController.java
@@ -1,15 +1,22 @@
 package com.soto.ecommerce.springecommerce.controller;
 
+import com.soto.ecommerce.springecommerce.model.Product;
 import com.soto.ecommerce.springecommerce.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private ProductService productService;
     @GetMapping("")
@@ -17,4 +24,14 @@ public class HomeController {
         model.addAttribute("products", productService.findAll());
         return "user/home";
     }
+    @GetMapping("producthome/{id}")
+    public String productHome(@PathVariable Integer id, Model model){
+        log.info("ID producto enviado como parametro {}", id);
+        Product product = new Product();
+        Optional<Product> productOptional = productService.get(id);
+        product = productOptional.get();
+        model.addAttribute("product", product);
+        return "user/producthome";
+    }
+
 }
