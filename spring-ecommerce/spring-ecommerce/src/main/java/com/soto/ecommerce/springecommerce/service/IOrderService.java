5abd482ca783de76fcab226a8d4ce14e2c1ebc1a
Jonatan Soto <jonatan.sotop.04@gmail.com>
2024-03-30 18:54:28 -0500

    Metodo para generar el numero de la orden

--- spring-ecommerce/spring-ecommerce/src/main/java/com/soto/ecommerce/springecommerce/service/IOrderService.java
+++ spring-ecommerce/spring-ecommerce/src/main/java/com/soto/ecommerce/springecommerce/service/IOrderService.java
@@ -2,6 +2,9 @@ package com.soto.ecommerce.springecommerce.service;
 
 import com.soto.ecommerce.springecommerce.model.Order;
 
+import java.util.List;
+
 public interface IOrderService {
+    List<Order> findAll();
     Order save(Order order);
 }
--- spring-ecommerce/spring-ecommerce/src/main/java/com/soto/ecommerce/springecommerce/service/OrderSeriviceImpl.java
+++ spring-ecommerce/spring-ecommerce/src/main/java/com/soto/ecommerce/springecommerce/service/OrderSeriviceImpl.java
@@ -5,6 +5,9 @@ import com.soto.ecommerce.springecommerce.repository.IOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrderSeriviceImpl implements  IOrderService{
     @Autowired
@@ -13,5 +16,35 @@ public class OrderSeriviceImpl implements  IOrderService{
     @Override
     public Order save(Order order) {
         return orderRepository.save(order);
+    }
+    @Override
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public String genererNumberOrder(){
+        int number = 0;
+        String numberConcatenated = "";
+        List<Order> orders = findAll();
+        List<Integer> numbers = new ArrayList<Integer>();
+        orders.stream().forEach(o -> numbers.add(Integer.parseInt(o.getNumber())));
+        if(orders.isEmpty()){
+            number = 1;
+        }else {
+            number = numbers.stream().max(Integer::compare).get();
+            number++;
+        }
+
+        if(number<10){
+            numberConcatenated="000000000"+ String.valueOf(number);
+        }else if(number<100){
+            numberConcatenated="00000000"+ String.valueOf(number);
+        }else if(number<1000){
+            numberConcatenated="0000000"+ String.valueOf(number);
+        }else if(number<10000){
+            numberConcatenated="000000"+ String.valueOf(number);
+        }
+
+        return numberConcatenated;
     }
 }
